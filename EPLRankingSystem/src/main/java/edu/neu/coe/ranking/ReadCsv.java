474d4021d84189f77b26faca1e60eb3b83d80881
Chao Ma <ma.cha@northeastern.edu>
2020-04-14 16:22:54 -0400

    detected OS and assign different path

--- EPLRankingSystem/src/main/java/edu/neu/coe/ranking/ReadCsv.java
+++ EPLRankingSystem/src/main/java/edu/neu/coe/ranking/ReadCsv.java
@@ -8,13 +8,14 @@ import java.util.Map;
 
  
 public class ReadCsv {
-	
-	String p1="src\\main\\resources\\EPL20152016.csv";
-	String p2="src\\main\\resources\\EPL20162017.csv";
-	String p3="src\\main\\resources\\EPL20172018.csv";
-	String p4="src\\main\\resources\\EPL20182019.csv";
-	String p5="src\\main\\resources\\EPL20192020.csv";
-	String[] paths= {p1,p2,p3,p4,p5};
+	String osName = System.getProperty("os.name");
+	boolean ifMac = osName.contains("Mac");
+	String p1 = ifMac ? "src/main/resources/EPL20152016.csv": "src\\main\\resources\\EPL20152016.csv";
+	String p2 = ifMac ? "src/main/resources/EPL20162017.csv": "src\\main\\resources\\EPL20162017.csv";
+	String p3 = ifMac ? "src/main/resources/EPL20172018.csv": "src\\main\\resources\\EPL20172018.csv";
+	String p4 = ifMac ? "src/main/resources/EPL20182019.csv": "src\\main\\resources\\EPL20182019.csv";
+	String p5 = ifMac ? "src/main/resources/EPL20192020.csv": "src\\main\\resources\\EPL20192020.csv";
+	String[] paths = {p1,p2,p3,p4,p5};
 	
     Map<String,Map<Integer,Integer>> hmap= read(2);
     Map<String,Map<Integer,Integer>> amap= read(3);  
